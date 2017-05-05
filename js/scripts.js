455ef8b7b0cb7aabd96c9877555afaace9a95be1
Antonio Molinari <molinari@incode.it>
2017-05-05 14:05:08 +0200

    music links

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  $('#fullpage').fullpage({
+    sectionsColor: ['#000000', '#f9f9f9', '#7BAABE', '#222222'],
+    //anchors: ['section1', 'section1', 'section2', '4thpage'],
+    menu: "#menu",
+    navigation: false
+  });
+  
   $('#toggle').click(function() {
   	$('body').toggleClass('off-canvas');
   });
